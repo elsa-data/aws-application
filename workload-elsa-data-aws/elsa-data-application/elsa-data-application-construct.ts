38bf4fc55cdcaac08e5d7a5755b86ceea3c71c46
Marko Malenic <mmalenic1@gmail.com>
2023-06-20 14:37:36 +1000

    fix: manual deployment and add dev config

--- workload-elsa-data-aws/elsa-data-application/elsa-data-application-construct.ts
+++ workload-elsa-data-aws/elsa-data-application/elsa-data-application-construct.ts
@@ -127,8 +127,6 @@ export class ElsaDataApplicationConstruct extends Construct {
           environment: {
             // we have a DSN that has no password or database name
             EDGEDB_DSN: props.edgeDbDsnNoPassword,
-            // we can choose the database name ourselves or default
-            EDGEDB_DATABASE: props.databaseName ?? "elsadata",
             // we don't do EdgeDb certs (our EdgeDb has made self-signed certs) so we must set this
             EDGEDB_CLIENT_TLS_SECURITY: "insecure",
             // environment variables set to setup the meta system for Elsa configuration
